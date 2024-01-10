59cb9361bed28dd12631356dcaf43349869f9537
oluwaborigbogbowan <isaacibigbemi@gmail.com>
2024-01-10 12:47:03 +0100

    removal of button

--- src/Product.js
+++ src/Product.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Product.css'
-import Button from '@mui/material/Button';
+
 
 
 
@@ -14,16 +14,14 @@ function Product({ id, name, image, description}) {
           <div className="product__info">
               <h2 className='product__name'>{name}</h2>
               <p className="product__description">Description:</p>
-              <p className='product__description__info'>{description.length <= 30 ? description :`${description.slice(0,30)}...` }</p>
+              <p className='product__description__info'>{description.length <= 90 ? description :`${description.slice(0,90)}...` }</p>
           </div>
-          <div className="product__buttons">
+          {/* <div className="product__buttons">
             <Button variant="contained"  sx={{background: '#0c6525', marginRight: '5px','&:hover': {backgroundColor: 'gray'}}}>Buy</Button>
             <Button variant="contained"sx={{background: '#0c6525',marginRight: '5px','&:hover': {backgroundColor: 'gray'}}}> Installment</Button>
             <Button variant="contained"sx={{background: '#0c6525','&:hover': {backgroundColor: 'gray'}}}>Rent</Button>
-              {/* <button>Buy now</button>
-              <button>Rent</button>
-              <button>Pay Installmentaly</button> */}
-          </div>
+              
+          </div> */}
       
        
     </div>
